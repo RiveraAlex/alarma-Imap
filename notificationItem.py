9dc52a2436c443d173f073aadebea6a3814a881b
Alex Joel Rivera <rivera.joel.alex@gmail.com>
2026-03-10 12:41:37 -0300

    Añadido de un trycatch para prevenir problemas con la hora

--- notificationItem.py
+++ notificationItem.py
@@ -26,20 +26,19 @@ class NotificationItem(QWidget):
         self.clicked.emit(self.data) 
     
     def isNew(self,time):
-        tz_ar = timezone(timedelta(hours=-3))
-        ahora = datetime.now(tz_ar)
-        # 2. Calculamos la diferencia   
-        fecha_obj = datetime.strptime(time, "%y/%m/%d %H:%M")
-        fecha_obj = fecha_obj.replace(tzinfo=tz_ar)
+        try:
+            tz_ar = timezone(timedelta(hours=-3))
+            ahora = datetime.now(tz_ar)  
+            fecha_obj = datetime.strptime(time, "%y/%m/%d %H:%M")
+            fecha_obj = fecha_obj.replace(tzinfo=tz_ar)
 
-        diferencia = ahora - fecha_obj
-        
-        # 3. Determinamos el estilo (Umbral: 30 minutos)
-        if diferencia.total_seconds() < 300:
-            # ESTILO NUEVO: Fondo ámbar suave con borde naranja
-            play_alarm_sound()
-            QApplication.alert(None,duration=3000)
-            return "border: 3px solid #e67e22; border-radius: 10px; background: #fef5e7;"
-        else:
-            # ESTILO ESTÁNDAR: El que ya tenías definido
+            diferencia = ahora - fecha_obj        
+            if diferencia.total_seconds() < 300:
+                play_alarm_sound()
+                QApplication.alert(None,duration=3000)
+                return "border: 3px solid #e67e22; border-radius: 10px; background: #fef5e7;"
+            else:
+                return "border: 2px solid #3498db; border-radius: 10px; background: #FFFFFF;"
+        except Exception as e:
+            print(f"DEBUG - Error en fecha del mail: {e}")
             return "border: 2px solid #3498db; border-radius: 10px; background: #FFFFFF;"
